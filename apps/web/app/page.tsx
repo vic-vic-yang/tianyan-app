ad049b63787787e89be76adcc2dae9b51291db93
vic-vic-yang <shao.bo.vic@gmail.com>
2024-12-17 16:11:52 +0800

    fix:build

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
+import { Button } from "@repo/ui/components/ui/button";
 import styles from "./page.module.css";
 
 type Props = Omit<ImageProps, "src"> & {
@@ -63,7 +63,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="web" className={styles.secondary}>
+        <Button className={styles.secondary}>
           Open alert
         </Button>
       </main>
