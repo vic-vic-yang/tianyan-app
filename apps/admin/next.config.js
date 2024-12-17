3aeae0bb8f46c51f7842083c0a67038db900477e
vic-vic-yang <shao.bo.vic@gmail.com>
2024-12-17 14:41:54 +0800

    fix: env

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-  },
 };
 
 export default nextConfig;
